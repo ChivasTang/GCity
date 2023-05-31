a6b79a16afdf9012a4ab3f5da8fe02fe4981f771
tangzh <tangzh1983@gmail.com>
2023-05-31 21:03:46 +0900

    change Request Log service to async.

--- RestAPI/Services/ReqLogService.fs
+++ RestAPI/Services/ReqLogService.fs
@@ -6,5 +6,8 @@ open RestAPI.Repositories
 type ReqLogService(_context: ApiDbContext, _reqLogRepository: IReqLogRepository) =
     interface IReqLogService with
         member this.SaveOne(reqLog) =
-            _reqLogRepository.Insert(reqLog) |> ignore
-            _context.SaveChanges() |> ignore
+            async {
+                _reqLogRepository.Insert(reqLog) |> ignore
+                _context.SaveChanges() |> ignore
+            }
+            |> Async.RunSynchronously
